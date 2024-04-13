6859e8cd09abd447af48c18b4d5e04f347acc055
gc-pereira <carvalho.gabrielp@gmail.com>
2024-04-13 15:23:12 -0300

    feat(Lamdba): Initial commit with raw quality code

---
+++ aws/lambda/quality/main.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# coding: utf-8
+import json
+import boto3
+import traceback
+import urllib.parse
+import logging as log
+from aws.utils import main as utils
+
+s3 = boto3.client('s3')
+
+
+log.basicConfig(level=log.INFO, format='[%(asctime)s][%(levelname)s] %(funcName)s() %(message)s', datefmt='%H:%M:%S')
+
+
+def source(event):
+    source = {
+            'event_time': '',
+            'database': s3_key(event).split('/')[0],
+            'type': s3_key(event).split('/')[1],
+            'sub': s3_key(event).split('/')[1]
+    }
+            
+    def s3_bucket(event):
+        return str(urllib.parse.unquote_plus(event['Records'][0]['s3']['bucket']['name'], encoding='utf-8'))
+    def s3_key(event):
+        return str(urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8'))
+
+    try:
+        source['s3_bucket'] = s3_bucket(event)
+        source['s3_key'] = s3_key(event)
+        source['s3_path'] = s3_bucket(event)+'/'+s3_key(event)
+        source['s3_uri'] = 's3://'+s3_bucket(event)+'/'+s3_key(event)
+        source['s3_file'] = s3_key(event).split('/')[-1]
+    except Exception as e:
+        print(str(event))
+        trace = traceback.format_exc()
+        log.error(e)
+        log.error(trace)
+        raise Exception("SourceParseError")
+    return source
+
+
+def get_keydict(bucket_name: str, key: str) -> dict:
+    response = s3.get_object(Bucket=bucket_name, Key=key)
+    json_content = response['Body'].read().decode('utf-8')
+    json_data = json.loads(json_content)
+    return json_data
+
+def idempotence(event_id):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(f"table")
+    try:
+        table.put_item(
+            Item = {
+                'event_id': event_id,
+                'expires': int(time.time()) + 60 # Expire the entry in 1 minute
+            },
+            ConditionExpression='attribute_not_exists(event_id)'
+        )
+        return True
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] != 'ConditionalCheckFailedException':
+            raise
+        else:
+            log.error('quality halted for repeat event_id: '+event_id)
+            return False
+    except Exception as e:
+        log.error('problem with data-quality lock dynamodb table')
+        print(str(e))
+        return False
+
+
+def load_data(src):
+    dfs = wr.s3.read_parquet(
+            src['s3_uri'],
+            use_threads=True,
+            chunked=100000
+        )
+    try:
+        for df in dfs:
+            test = df.dtypes # <--- random test to see if df fails
+            break
+        dfs = wr.s3.read_parquet(
+            src['s3_uri'],
+            use_threads=True,
+            chunked=100000
+        )
+    except ArrowInvalid as e:
+        log.warning(str(e))
+        log.warning("fallback read with safe cast = False")
+        dfs = wr.s3.read_parquet(
+            src['s3_uri'],
+            use_threads=True,
+            chunked=100000,
+            safe=False
+        )
+    return dfs
+    
+
+def quality():
+    
+
+
+def handler(event, context):
+    if utils.in_aws():
+        if any(utils.in_lambda(), utils.in_glue()):
+            src = source(event)
+            if idempotence(src['s3_uri']):
+                src = {**src, **quality(src)}
+    else:
+        return False
---
+++ aws/utils/main.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python
+# coding: utf-8
+import os
+import boto3
+import traceback
+import urllib.parse
+import pandas as pd
+import logging as log
+import awswrangler as wr
+
+from datetime import datetime
+from typing import Iterable, Any, Tuple
+
+
+log.basicConfig(level=log.INFO, format='[%(asctime)s][%(levelname)s] %(funcName)s() %(message)s', datefmt='%H:%M:%S')
+
+
+def in_aws():
+    """It can be used to check whether the code is running in a local environment or public cloud
+
+    Returns:
+        bool: returns True if this code is running in aws or False otherwise.
+    """
+    try:
+        return os.name != 'nt'
+    except Exception as e:
+        print('in_aws() '+str(e))
+        return False
+
+
+def in_lambda():
+    """It can be used to check whether the code is running in AWS Lambda function
+
+    Returns:
+        bool: returns True if this code is running in lambda function or False otherwise.
+    """
+    try:
+        in_lambda = os.environ["AWS_EXECUTION_ENV"]
+        if in_lambda is not None:
+            return True
+        else:
+            return False
+    except KeyError:
+        return False
+    except Exception as e:
+        print('in_lambda() '+str(e))
+        return False
+
+
+def in_glue():
+    """It can be used to check whether the code is running in AWS Lambda function
+
+    Returns:
+        bool: returns True if this code is running in lambda function or False otherwise.
+    """
+    return in_aws() and not in_lambda()
+
+
+def get_env():
+    """It can be used to get environment context
+
+    Returns:
+        str: returns string 'prd' for production or 'dev' to development environments.
+    """
+    try:
+        return wr.sts.get_current_identity_arn().split('ht8-')[1].split('-')[0]
+    except IndexError:
+        return 'prd'
+    except Exception as e:
+        print('get_env() '+str(e))
+        return 'prd'
+
+
+def get_account_id():
+    """It can be used to get amazon web services account number
+
+    Returns:
+        str: returns string with account number.
+    """
+    current_env = get_env()
+    if current_env == 'dev':
+        return '111122223333'
+    elif current_env == 'prd':
+        return '444555566666'
+    else:
+        print('ERROR !-> get_account_id() no account id for env: ', current_env)
+        raise SystemExit
+
+
+
+def boto3_assumed_role_client(resource, role_arn):
+    """
+    Create a boto3 client from an assumed role
+    """
+    sts_connection = boto3.client('sts')
+    acct_b = sts_connection.assume_role(
+        RoleArn=role_arn,
+        RoleSessionName="cross_acct_lambda"
+    )
+
+    ACCESS_KEY = acct_b['Credentials']['AccessKeyId']
+    SECRET_KEY = acct_b['Credentials']['SecretAccessKey']
+    SESSION_TOKEN = acct_b['Credentials']['SessionToken']
+
+    client = boto3.client(
+        resource,
+        aws_access_key_id=ACCESS_KEY,
+        aws_secret_access_key=SECRET_KEY,
+        aws_session_token=SESSION_TOKEN,
+    )
+    return client
+
+
+def boto3_assumed_role_session(role_arn: str) -> boto3.Session:
+    """
+    Create a boto 3 session from an assumed role
+    """
+    sts_connection = boto3.client('sts')
+    acct_b = sts_connection.assume_role(
+        RoleArn=role_arn,
+        RoleSessionName="cross_acct_lambda")
+    ACCESS_KEY = acct_b['Credentials']['AccessKeyId']
+    SECRET_KEY = acct_b['Credentials']['SecretAccessKey']
+    SESSION_TOKEN = acct_b['Credentials']['SessionToken']
+    session = boto3.session.Session(
+        aws_access_key_id=ACCESS_KEY,
+        aws_secret_access_key=SECRET_KEY,
+        aws_session_token=SESSION_TOKEN,
+        region_name='us-east-1'
+    )
+    return session
+
+
+def boto3_assumed_role_default_session(role_arn: str) -> boto3.Session:
+    """
+    Set default boto3 session from an assumed role
+    """
+    sts_connection = boto3.client('sts')
+    acct_b = sts_connection.assume_role(
+        RoleArn=role_arn,
+        RoleSessionName="cross_acct_lambda")
+    ACCESS_KEY = acct_b['Credentials']['AccessKeyId']
+    SECRET_KEY = acct_b['Credentials']['SecretAccessKey']
+    SESSION_TOKEN = acct_b['Credentials']['SessionToken']
+    boto3.setup_default_session(
+        aws_access_key_id=ACCESS_KEY,
+        aws_secret_access_key=SECRET_KEY,
+        aws_session_token=SESSION_TOKEN,
+        region_name='us-east-1'
+    )
+
+
+def signal_last(it:Iterable[Any]) -> Iterable[Tuple[bool, Any]]:
+    iterable = iter(it)
+    ret_var = next(iterable)
+    for val in iterable:
+        yield False, ret_var
+        ret_var = val
+    yield True, ret_var
+
+
+def add_process_dates(df, src):
+    df['data_fonte'] = src['date']
+    df['data_carga'] = datetime.now()
+    return df
